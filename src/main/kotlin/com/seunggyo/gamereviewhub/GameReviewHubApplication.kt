8ef0e072de1026ede327ce67208384c951d8319d
KIM <tmdry99@gmail.com>
2025-03-07 00:48:44 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/seunggyo/gamereviewhub/GameReviewHubApplication.kt
@@ -0,0 +1,11 @@
+package com.seunggyo.gamereviewhub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameReviewHubApplication
+
+fun main(args: Array<String>) {
+    runApplication<GameReviewHubApplication>(*args)
+}
---
+++ src/test/kotlin/com/seunggyo/gamereviewhub/GameReviewHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seunggyo.gamereviewhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameReviewHubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
